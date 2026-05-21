3e1012a6ebc3e993553985f40317466f9bc7255c
Krishan Limbu <nikitlimbu006@gmail.com>
2026-05-21 12:08:01 -0400

    Finsih

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -22,14 +22,11 @@ module.exports = {
             }
         ],
     },
-    // optimization: {
-    //     minimize: true,
-    // },
     plugins: [
         new webpack.DefinePlugin({
             "process.env": {
                 //This has effect on the react lib size
-                NODE_ENV: JSON.stringify("development"),
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV || "development"),
             },
         }),
     ],
--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -3,4 +3,4 @@ from decouple import config
 CLIENT_ID = "0dc78e7e535d471b82edcd64c7902016"
 CLIENT_SECRET = config("CLIENT_SECRET")
 
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/spotify-callback/"
+REDIRECT_URI = config("REDIRECT_URI")
